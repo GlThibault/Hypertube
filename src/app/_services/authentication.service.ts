2e391c14693a713e7bc8ed25cd3270695d6c4e8a
Thibault GLANDAIS <tglandai@student.42.fr>
2017-04-07 21:55:17 +0200

    comment

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -7,22 +7,19 @@ import { AppConfig } from '../app.config';
 
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: Http, private config: AppConfig) { }
+  constructor(private http: Http, private config: AppConfig) { }
 
-    login(username: string, password: string) {
-        return this.http.post(this.config.apiUrl + '/users/authenticate', { username: username, password: password })
-            .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                let user = response.json();
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
-                }
-            });
-    }
+  login(username: string, password: string) {
+    return this.http.post(this.config.apiUrl + '/users/authenticate', { username: username, password: password })
+      .map((response: Response) => {
+        let user = response.json();
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
+      });
+  }
 
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-    }
+  logout() {
+    localStorage.removeItem('currentUser');
+  }
 }
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -6,36 +6,33 @@ import { User } from '../_models/index';
 
 @Injectable()
 export class UserService {
-    constructor(private http: Http, private config: AppConfig) { }
+  constructor(private http: Http, private config: AppConfig) { }
 
-    getAll() {
-        return this.http.get(this.config.apiUrl + '/users', this.jwt()).map((response: Response) => response.json());
+  getAll() {
+    return this.http.get(this.config.apiUrl + '/users', this.jwt()).map((response: Response) => response.json());
+  }
+
+  getById(_id: string) {
+    return this.http.get(this.config.apiUrl + '/users/' + _id, this.jwt()).map((response: Response) => response.json());
+  }
+
+  create(user: User) {
+    return this.http.post(this.config.apiUrl + '/users/register', user, this.jwt());
+  }
+
+  update(user: User) {
+    return this.http.put(this.config.apiUrl + '/users/' + user._id, user, this.jwt());
+  }
+
+  delete(_id: string) {
+    return this.http.delete(this.config.apiUrl + '/users/' + _id, this.jwt());
+  }
+
+  private jwt() {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.token) {
+      let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
+      return new RequestOptions({ headers: headers });
     }
-
-    getById(_id: string) {
-        return this.http.get(this.config.apiUrl + '/users/' + _id, this.jwt()).map((response: Response) => response.json());
-    }
-
-    create(user: User) {
-        return this.http.post(this.config.apiUrl + '/users/register', user, this.jwt());
-    }
-
-    update(user: User) {
-        return this.http.put(this.config.apiUrl + '/users/' + user._id, user, this.jwt());
-    }
-
-    delete(_id: string) {
-        return this.http.delete(this.config.apiUrl + '/users/' + _id, this.jwt());
-    }
-
-    // private helper methods
-
-    private jwt() {
-        // create authorization header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
-    }
+  }
 }
