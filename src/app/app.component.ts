fa7df97b05027e155098a451f27898cdd6fae222
Geoffrey PICARD <gepicard@e2r4p11.42.fr>
2017-05-29 15:31:26 +0200

    Front rework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./bootstrap.css', './app.component.css']
 })
 
 export class AppComponent {
