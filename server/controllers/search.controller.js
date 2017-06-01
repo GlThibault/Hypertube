ce013c5de26a6b025fc3aab9e1d1882eb70eb74c
Thibault GLANDAIS <tglandai@student.42.fr>
2017-06-01 13:14:21 +0200

    isohunt api search

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -2,24 +2,63 @@
 
 const express = require('express');
 const router = express.Router();
-const PirateBay = require('thepiratebay');
+const isohuntApi = require('isohunt-api');
+const PirateBayAPI = require('thepiratebay');
 const movieService = require('../services/movie.service');
 
+const compare = (a, b) => {
+  if (parseInt(a.seeders) < parseInt(b.seeders))
+    return 1;
+  if (parseInt(a.seeders) > parseInt(b.seeders))
+    return -1;
+  return 0;
+};
+
+const mySort = (tpb, isoh) => {
+  isoh.forEach((element) => {
+    tpb.push(element);
+  }, this);
+  tpb.sort(compare);
+  return tpb;
+};
+
 router.post('/', (req, res) => {
-  PirateBay.search(req.body.searchquery.search, {
-      category: 'video',
-      page: 0,
-      orderBy: 'seeds',
-      sortBy: 'desc'
+  isohuntApi.search(req.body.searchquery.search, {
+      category: 'movies',
+      order: 'seeders'
+    }).then(isohuntResults => {
+      PirateBayAPI.search(req.body.searchquery.search, {
+          category: 'movies',
+          page: 0,
+          orderBy: 'seeds',
+          sortBy: 'desc'
+        })
+        .then(TPBResults => {
+          movieService.imdb(mySort(TPBResults, isohuntResults), data => {
+            res.send(data);
+          });
+        })
+        .catch(err => {
+          res.status(400).send(err);
+        });
     })
-    .then(results => movieService.imdb(results, data => res.send(data)))
-    .catch(err => res.status(400).send(err));
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 router.post('/top', (req, res) => {
-  PirateBay.topTorrents(200)
-    .then(results => movieService.imdb(results, data => res.send(data)))
-    .catch(err => res.status(400).send(err));
+  isohuntApi.search('x264', {
+    category: 'movies',
+    order: 'seeders'
+  }).then(isohuntResults => {
+    PirateBayAPI.topTorrents(200)
+      .then(TPBResults => {
+        let results = mySort(TPBResults, isohuntResults);
+        movieService.imdb(results, data => res.send(data));
+      })
+      .catch(err => res.status(400).send(err));
+  });
 });
 
 module.exports = router;
