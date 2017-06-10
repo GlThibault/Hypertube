08577197384ac05abf2a5e5c46b605d7201ecc70
Geoffrey PICARD <gepicard@e3r4p11.42.fr>
2017-06-10 13:45:21 +0200

    front tri

---
+++ src/app/_services/tri.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { AppConfig } from '../app.config';
+
+@Injectable()
+export class TriService {
+
+}
--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Movie } from '../_models/movie';
 import { SearchService } from '../_services/index';
+import { TriService } from '../_services/index';
 
 @Component({
   selector: 'app-library',
@@ -15,6 +16,10 @@ export class LibraryComponent implements OnInit {
 
   constructor(
     private searchService: SearchService) { }
+
+    tri_nom () {
+      console.log(this.movies[0]);
+    }
 
   ngOnInit() {
     this.loading = true;
